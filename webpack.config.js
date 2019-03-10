90e9796bc70d0ffe759299f9b5e818acfd0258f9
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2019-03-10 00:11:27 -0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: 'https://lautaumpierrez.github.io/docs/',
+    publicPath: '/',
     filename: 'build.js'
   },
   module: {
