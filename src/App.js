bcc9a6499dac9f4d98433ab39d30737631304353
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2019-06-30 00:48:45 -0300

    starting the new version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {HashRouter, Switch, Route} from 'react-router-dom';
-import logo from './logo.svg';
 import HomePage from './pages/Home';
-import BiographyPage from './pages/Biography';
-import PortfolioPage from './pages/Portfolio';
-import ContactPage from './pages/Contact';
+
 import './App.scss';
 import 'bulma/css/bulma.min.css';
 import 'animate.css/animate.min.css';
@@ -14,9 +11,6 @@ function App() {
       <HashRouter>
         <Switch>
           <Route path='/' component={HomePage} exact></Route>
-          <Route path='/biography' component={BiographyPage} exact></Route>
-            <Route path='/portfolio' component={PortfolioPage} exact></Route>
-          <Route path='/contact' component={ContactPage} exact></Route>
         </Switch>
       </HashRouter>
     </div>
